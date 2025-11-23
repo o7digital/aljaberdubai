b9e8a99b04e6bc0acf71498aa96dd1fbad294046
o7digital <101292703+o7digital@users.noreply.github.com>
2025-11-23 01:04:26 -0600

    Update hero banner title to ALJABER DUBAI and add slider images

--- src/components/homes/home-one/Banner.tsx
+++ src/components/homes/home-one/Banner.tsx
@@ -1,18 +1,77 @@
 "use client"
+import { useEffect, useRef } from "react"
 import Image from "next/image"
-import DropdownOne from "@/components/search-dropdown/home-dropdown/DropdownOne";
-
+import Slider from "react-slick"
+import DropdownOne from "@/components/search-dropdown/home-dropdown/DropdownOne"
 import titleShape from "@/assets/images/shape/shape_01.svg"
-import bannerThumb from "@/assets/images/assets/ils_01.svg"
 
 const Banner = () => {
+   const sliderRef = useRef<Slider | null>(null)
+
+   const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 1000,
+      fade: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+   }
+
+   useEffect(() => {
+      const slider = sliderRef.current
+      if (slider) {
+         slider.slickPlay()
+      }
+   }, [])
+
    return (
-      <div className="hero-banner-one bg-pink z-1 pt-225 xl-pt-200 pb-250 xl-pb-150 lg-pb-100 position-relative">
-         <div className="container position-relative">
+      <div className="hero-banner-one z-1 pt-225 xl-pt-200 pb-250 xl-pb-150 lg-pb-100 position-relative" style={{ overflow: 'hidden' }}>
+         {/* Background Slider */}
+         <div className="hero-slider-wrapper position-absolute w-100 h-100" style={{ top: 0, left: 0, zIndex: 0 }}>
+            <Slider ref={sliderRef} {...settings}>
+               <div className="slider-item">
+                  <div className="slider-bg" style={{
+                     backgroundImage: 'url(/slider/dubai1.jpg)',
+                     backgroundSize: 'cover',
+                     backgroundPosition: 'center',
+                     width: '100%',
+                     height: '100%',
+                     position: 'absolute',
+                     top: 0,
+                     left: 0
+                  }} />
+               </div>
+               <div className="slider-item">
+                  <div className="slider-bg" style={{
+                     backgroundImage: 'url(/slider/dubai2.jpg)',
+                     backgroundSize: 'cover',
+                     backgroundPosition: 'center',
+                     width: '100%',
+                     height: '100%',
+                     position: 'absolute',
+                     top: 0,
+                     left: 0
+                  }} />
+               </div>
+            </Slider>
+         </div>
+
+         {/* Overlay pour améliorer la lisibilité du texte */}
+         <div className="position-absolute w-100 h-100" style={{ 
+            top: 0, 
+            left: 0, 
+            background: 'rgba(255, 255, 255, 0.85)',
+            zIndex: 1 
+         }} />
+
+         <div className="container position-relative" style={{ zIndex: 2 }}>
             <div className="row">
                <div className="col-xxl-10 col-xl-9 col-lg-10 col-md-10 m-auto">
-                  <h1 className="hero-heading text-center wow fadeInUp">Get the ideal home for your <span className="d-inline-block position-relative">family <Image src={titleShape} alt="" className="lazy-img" /></span></h1>
-                  <p className="fs-24 color-dark text-center pt-35 pb-35 wow fadeInUp" data-wow-delay="0.1s">We’ve more than 745,000 apartments, place & plot.</p>
+                  <h1 className="hero-heading text-center wow fadeInUp">ALJABER <span className="d-inline-block position-relative">DUBAI <Image src={titleShape} alt="" className="lazy-img" /></span></h1>
+                  <p className="fs-24 color-dark text-center pt-35 pb-35 wow fadeInUp" data-wow-delay="0.1s">We've more than 745,000 apartments, place & plot.</p>
                </div>
             </div>
             <div className="row">
@@ -25,7 +84,6 @@ const Banner = () => {
                </div>
             </div>
          </div>
-         <Image src={bannerThumb} alt="" className="lazy-img shapes w-100 illustration" />
       </div>
    )
 }
