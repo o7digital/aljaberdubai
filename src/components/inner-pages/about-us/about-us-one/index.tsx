ea0f1ba7d9928d9b587d0390b1a46702e024a907
o7digital <101292703+o7digital@users.noreply.github.com>
2025-11-23 20:11:38 -0600

    feat: traduction titre breadcrumb About Us - Qui Sommes-Nous

--- src/components/inner-pages/about-us/about-us-one/index.tsx
+++ src/components/inner-pages/about-us/about-us-one/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import BreadcrumbOne from "@/components/common/breadcrumb/BreadcrumbOne"
 import HeaderOne from "@/layouts/headers/HeaderOne"
 import BLockFeatureOne from "./BLockFeatureOne"
@@ -8,12 +9,15 @@ import AgentArea from "@/components/homes/home-one/AgentArea"
 import Brand from "./Brand"
 import FooterOne from "@/layouts/footers/FooterOne"
 import FancyBanner from "@/components/common/FancyBanner"
+import { useTranslation } from "@/contexts/TranslationContext"
 
 const AboutUsOne = () => {
+   const { t } = useTranslation();
+   
    return (
       <>
          <HeaderOne style={true} />
-         <BreadcrumbOne title="About Agency" sub_title="About us" style={false} />
+         <BreadcrumbOne title={t('aboutUs.breadcrumbTitle')} sub_title={t('aboutUs.subtitle')} style={false} />
          <BLockFeatureOne />
          <VideoBanner />
          <BLockFeatureFive style={true} />
