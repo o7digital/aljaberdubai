615bcd7a36499a1136e64efe6610fda9acdaba87
o7digital <101292703+o7digital@users.noreply.github.com>
2025-11-23 19:55:50 -0600

    feat: contenu About Us traduit FR/EN avec présentation Aljaber Dubai

--- src/components/inner-pages/about-us/about-us-one/BLockFeatureOne.tsx
+++ src/components/inner-pages/about-us/about-us-one/BLockFeatureOne.tsx
@@ -1,30 +1,14 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
+import { useTranslation } from "@/contexts/TranslationContext"
 
 import titleShape from "@/assets/images/shape/title_shape_06.svg";
 import Count from "@/components/common/Count";
 
-interface ContentType {
-   sub_title: string;
-   desc_1: JSX.Element;
-   title_1: string;
-   title_2: string;
-   desc_2: JSX.Element;
-   desc_3: JSX.Element;
-}
-
-const feature_content: ContentType = {
-   sub_title: "About us",
-   desc_1: (<>Our founders Dustin Moskovitz and Justin Rosenstein met while leading Engineering .</>),
-   title_1: "Who we are?",
-   title_2: "Our Mission",
-   desc_2: (<>Our founders Dustin Moskovitz & Justin Rosenstein met  leading Engineering teams at Facebook. As operations scaled, they grew frustrated by how difficult coordinate</>),
-   desc_3: (<>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod incididunt ut labore et dolore aliqua.</>),
-}
-
-const { sub_title, desc_1, title_1, title_2, desc_2, desc_3 } = feature_content;
-
 const BLockFeatureOne = () => {
+   const { t } = useTranslation();
+
    return (
       <div className="block-feature-two mt-150 xl-mt-100">
          <div className="container">
@@ -32,23 +16,23 @@ const BLockFeatureOne = () => {
                <div className="col-lg-6 wow fadeInLeft">
                   <div className="me-xxl-4">
                      <div className="title-one mb-60 lg-mb-40">
-                        <div className="upper-title">{sub_title}</div>
-                        <h3>Secure your <span>family&apos;s<Image src={titleShape} alt="" className="lazy-img" /></span> Dream home.</h3>
-                        <p className="fs-22">{desc_1}</p>
+                        <div className="upper-title">{t('aboutUs.subtitle')}</div>
+                        <h3>{t('aboutUs.mainTitle')} <span>{t('aboutUs.familyWord')}<Image src={titleShape} alt="" className="lazy-img" /></span> {t('aboutUs.dreamHome')}</h3>
+                        <p className="fs-22">{t('aboutUs.intro')}</p>
                      </div>
-                     <Link href="/contact" className="btn-two">Contact Us</Link>
+                     <Link href="/contact" className="btn-two">{t('aboutUs.contactButton')}</Link>
                      <div className="counter-wrapper border-top pt-40 md-pt-10 mt-65 md-mt-40">
                         <div className="row">
                            <div className="col-xxl-6 col-sm-5">
                               <div className="counter-block-one mt-20">
-                                 <div className="main-count fw-500 color-dark"><span className="counter"><Count number={1.2} /></span>%</div>
-                                 <span>Low interest rate</span>
+                                 <div className="main-count fw-500 color-dark"><span className="counter"><Count number={15} /></span>+</div>
+                                 <span>{t('aboutUs.yearsExperience')}</span>
                               </div>
                            </div>
                            <div className="col-xxl-6 col-sm-7">
                               <div className="counter-block-one mt-20">
-                                 <div className="main-count fw-500 color-dark">$<span className="counter"><Count number={1.3} /></span>b+</div>
-                                 <span>Cumulative trading volume</span>
+                                 <div className="main-count fw-500 color-dark"><span className="counter"><Count number={500} /></span>+</div>
+                                 <span>{t('aboutUs.propertiesSold')}</span>
                               </div>
                            </div>
                         </div>
@@ -59,10 +43,10 @@ const BLockFeatureOne = () => {
                <div className="col-lg-6 wow fadeInRight">
                   <div className="block-two md-mt-40">
                      <div className="bg-wrapper">
-                        <h5>{title_1}</h5>
-                        <p className="fs-22 lh-lg mt-20">{desc_2}</p>
-                        <h5 className="top-line">{title_2} </h5>
-                        <p className="fs-22 lh-lg mt-20">{desc_3}</p>
+                        <h5>{t('aboutUs.whoWeAre')}</h5>
+                        <p className="fs-22 lh-lg mt-20">{t('aboutUs.whoWeAreText')}</p>
+                        <h5 className="top-line">{t('aboutUs.ourMission')}</h5>
+                        <p className="fs-22 lh-lg mt-20">{t('aboutUs.ourMissionText')}</p>
                      </div>
                   </div>
                </div>
